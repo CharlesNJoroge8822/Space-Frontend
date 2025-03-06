89860e6b6d2a36658fa5c9c48b3773071e098762
Marion Nabulobi <arinabulobi@gmail.com>
2025-03-06 09:06:54 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ export default function Login() {
         }
 
         try {
-            const response = await fetch("https://space-backend-9.onrender.com/request_password_reset", {
+            const response = await fetch("https://space-backend-2-p4kd.onrender.com/request_password_reset", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email: resetEmail }),
